8c4d1bf5292e8a2006973f326c133a42c1bc485d
Daniele Basile <asterix24@gmail.com>
2014-01-14 08:46:19 +0100

    Animate sheep..

--- pycugame.py
+++ pycugame.py
@@ -11,6 +11,7 @@ py.init()
 FPS = 30 # frames per second setting
 fpsClock = py.time.Clock()
 
+py.display.set_caption('PyCuGame')
 screen=py.display.set_mode((640,480),0,32)
 bg=py.image.load(bg_file).convert()
 ch=py.image.load(ch_file)
@@ -20,6 +21,7 @@ pyi=10
 py.draw.rect(bg,(255,0,0),Rect((300,300),(10,80)))
 x,y=0,298
 move_x,move_y=0,0
+
 while True:
     for event in py.event.get():
         if event.type==QUIT:
@@ -59,8 +61,8 @@ while True:
 
 
 
-    px += 1
-    if px < 480:
+    px += 5
+    if px >= 480:
         px = 10
 
     screen.fill((0,0,0))
@@ -69,6 +71,7 @@ while True:
     screen.blit(pycu,(px,pyi))
     py.display.update()
     fpsClock.tick(FPS)
+
 '''per far "sparare" prova in event.key== tasto a caso, genera
  un cerchio e lo fa muovere lungo lo schermo'''
 
