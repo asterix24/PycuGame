25340deb64ea2b3267c98b14c2d0ac062fd7d91b
hidefix <hidefix92@gmail.com>
2014-03-15 20:21:52 +0100

    Tolti spazi.

--- pycugame.py
+++ pycugame.py
@@ -4,19 +4,19 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 # Copyright 2014 Daniele Basile <asterix24@gmail.com>
-#                Francesca Basile <hidefix92@gmail.com>
-# 
+#               Francesca Basile <hidefix92@gmail.com>
+#
 # \author Daniele Basile <asterix24@gmail.com>
 # \author Francesca Basile <hidefix92@gmail.com>
 #
@@ -51,7 +51,7 @@ def init():
 def event_mgr(context):
 	for event in context:
 		# usciamo con ESC e q
-		if event.type == QUIT or (event.type == KEYDOWN and 
+		if event.type == QUIT or (event.type == KEYDOWN and
 				(event.key == K_ESCAPE or event.key == K_q)):
 			pygame.quit()
 			sys.exit()
