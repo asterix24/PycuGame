0c0f1df59ce993227fcf0c60c8d1ff4bb04282c1
Daniele Basile <asterix24@gmail.com>
2014-05-07 22:24:33 +0200

    Prova di commit.

--- pycugame.py
+++ pycugame.py
@@ -67,6 +67,7 @@ def pycugame():
 	font.draw_text("Pycu Game! 3", cfg.RED, Pos(200, 200))
 	font.draw_text("Pycu Game! 4", cfg.WHITE, Pos(400, 200))
 	font.blit_text("Pycu Game! 5", cfg.GREEN, Pos(50, 50), 500)
+	font.blit_text("Pycu Game! 6", cfg.GREEN, Pos(52, 52), 500)
 
 	while True:
 		background.fill(cfg.BLACK)
